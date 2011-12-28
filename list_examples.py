325b914e04a7fe5614405dd4ef6f94c023304af2
Kellan Jacobs <kellankade@gmail.com>
2011-12-27 21:07:54 -0800

    Updated List Examples

--- list_examples.py
+++ list_examples.py
@@ -9,24 +9,45 @@ Created by Kellan Jacobs on 2011-12-10.
 def main():
     # Create a list of numbers 1 - 10
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+    unsorted = [4, 2, 9, 6, 7, 1]
+    mixed = ['Kellan', 36, ['a', 'b']]
+    who = 'You never forget your first doctor'.split()
 
-    # Show the list we are starting with. Note the comma at the end. I used
-    # this because simple print can't add a list to a string and I wanted
-    # them to print on the same line.
-    print "Print the list:",
-    print numbers
 
     # Slicing works for lists too
-    print 'Slicing works:',
-    print numbers[3:5]
+    print 'Slicing works: %s' % (numbers[3:5])
 
     # Print every other item in a list
-    print 'Print Every other item:',
-    print numbers[::2]
+    print 'Print Every other item: %s' % (numbers[::2])
 
-    # Print the evens
-    print 'Print the Even numbers:',
-    print numbers[1::2]
+
+    # number of items in list
+    print "Items #: %s" % (len(numbers))
+
+    # Assignment to list postition
+    print 'Mixed List: ' + str(mixed)
+    mixed[2] = 'Jacobs'
+    print 'Mixed List after assignment: %s' % (mixed)
+
+    # Delete item from list
+    del mixed[1]
+    print 'After Delete: %s' % (mixed)
+
+    mixed.append('DOB: 11/05/1975')
+    print 'Adding to list: %s' % (mixed)
+
+    # Find first occurance of a list item
+    print 'Who List: %s' % (who)
+    print 'forget occurs at: %s' % (who.index('forget'))
+
+    # Insert into a list
+    mixed.insert( 1, 'Daniel')
+    print 'Insert into list: %s' % (mixed)
+
+    # Sort a list
+    unsorted.sort()
+    print 'Sorted List: %s' % (unsorted)
+
 
 
 if __name__ == '__main__':

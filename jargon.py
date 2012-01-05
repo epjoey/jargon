1ec7190d35cb9eedb6058ceef0b845522f0154c9
Kellan Jacobs <kellan@titanmedia.com>
2012-01-04 16:06:43 -0800

    Jargon Game

--- jargon.py
+++ jargon.py
@@ -7,18 +7,45 @@ text = f.read()
 words = text.split()
 f.close()
 
-wordChoice = choice(words)
-print wordChoice
+mysteryWord = choice(words)
 
-choice = 0
+board = list()
+board.append(mysteryWord[0])
+answer = ['-', '-', '-', '-', '-']
+
+for i in range(4):
+	board.append('*')
+
+print ' ' * 10 + '+' + '-' * 48 + '+'
+print ' ' * 10 + '|' + "Welcome to Jargon".center(48) + '|'
+print ' ' * 10 + '+' + '-' * 48 + '+'
 
 while True:
-	spots = list('*' * 5)
-	spots[0]=wordChoice[0]
-	for spot in spots:
-		print spot,
-	choice += 1
-	if (choice > 5):
+	print board
+	guess = raw_input('Enter your guess: ')
+	if not guess: break
+
+	# Check for Valid Word
+	if guess not in words:
+		print "You entered an invalid word"
+		continue
+
+	if guess == mysteryWord:
+		print "You guessed the right word"
 		break
+	
+	for i in range(len(guess)):
+		if mysteryWord[i] == guess[i]:
+			board[i] = guess[i]
+			answer[i] = guess[i]
+		elif guess[i] in mysteryWord:
+			answer[i] = '+'
+		else:
+			answer[i] = '-'
+	print "Your Results".center(20)
+	print '-'*20 
+	print answer
+	print "Try Again".center(20)
+	print '-'*20 
 
 
